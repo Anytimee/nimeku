1b6e1d56eb8e7613aa8dca6e8cc03aef2bfd7cc1
anytimee <unlimitedpowergirl@gmail.com>
2025-01-18 18:58:53 +0700

    Menambahkan direktori baru di Laravel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build", // Atur direktori output sesuai dengan yang Vercel harapkan
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.jsx"], // Masukkan app.js dan app.css
